53242271ea9b18417d7a993d287204b764d1fa21
Alexander Gould <alexander.gould94@gmail.com>
2021-05-04 18:08:40 -0700

    commit tree b48e1ca15d9dd268c7c6dec6ce598cc037b1d37b

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    set :views, settings.root + '/4:26-4:30/sinatra-nested-forms/views'
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+    
   end
 end
